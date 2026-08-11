2ab5926a12794dbc89fa8cf9fca0bf16c000e855
Valentingiaccone <163468423+Valentingiaccone@users.noreply.github.com>
2026-08-11 14:14:59 -0300

    probar a domino

--- src/rooms/Personajes.ts
+++ src/rooms/Personajes.ts
@@ -349,22 +349,23 @@ export class GestorPersonajes {
     private personajes: Record<string, IPersonaje> = {};
 
     constructor() {
-        this.registrar(new ColeCasiddy());
-        this.registrar(new Berry());
-        this.registrar(new Maton());
-        this.registrar(new Mandy());
-        this.registrar(new Tralalero());
-        this.registrar(new Darryl());
-        this.registrar(new JetpackCat());
-        this.registrar(new KayFaraday());
-        this.registrar(new Chester());
-        this.registrar(new Frank());
-        this.registrar(new Pam());
-        this.registrar(new Trucy());
-        this.registrar(new HongoUp());
+        // this.registrar(new ColeCasiddy());
+        // this.registrar(new Berry());
+        // this.registrar(new Maton());
+        // this.registrar(new Mandy());
+        // this.registrar(new Tralalero());
+        // this.registrar(new Darryl());
+        // this.registrar(new JetpackCat());
+        // this.registrar(new KayFaraday());
+        // this.registrar(new Chester());
+        // this.registrar(new Frank());
+        // this.registrar(new Pam());
+        // this.registrar(new Trucy());
+        // this.registrar(new HongoUp());
         this.registrar(new Hongo());
         this.registrar(new Lesly());
         this.registrar(new Mikotoba());
+        this.registrar(new Domino())
     }
 
     private registrar(p: IPersonaje) {
