3ec961a8e54c97531b1b8c752cf8016e368e4161
Naniboro547 <adrianchacaliazaf@gmail.com>
2026-08-21 20:41:53 -0500

    laiksitereolvidaste
    lilpeep

--- src/rooms/Personajes.ts
+++ src/rooms/Personajes.ts
@@ -859,32 +859,32 @@ export class GestorPersonajes {
     private personajes: Record<string, IPersonaje> = {};
 
     constructor() {
-        // this.registrar(new ColeCasiddy());
-        // this.registrar(new Berry());
-        // this.registrar(new Maton());
-        // this.registrar(new Mandy());
-        // this.registrar(new Tralalero());
-        // this.registrar(new Darryl());
-        // this.registrar(new JetpackCat());
-        // this.registrar(new KayFaraday());
+        this.registrar(new ColeCasiddy());
+        this.registrar(new Berry());
+        this.registrar(new Maton());
+        this.registrar(new Mandy());
+        this.registrar(new Tralalero());
+        this.registrar(new Darryl());
+        this.registrar(new JetpackCat());
+        this.registrar(new KayFaraday());
         this.registrar(new Chester());
-        // this.registrar(new Frank());
-        // this.registrar(new Pam());
-        // this.registrar(new Trucy());
-        // this.registrar(new HongoUp());
-        // this.registrar(new Hongo());
-        // this.registrar(new Lesly());
-        // this.registrar(new Mikotoba());
+        this.registrar(new Frank());
+        this.registrar(new Pam());
+        this.registrar(new Trucy());
+        this.registrar(new HongoUp());
+        this.registrar(new Hongo());
+        this.registrar(new Lesly());
+        this.registrar(new Mikotoba());
         this.registrar(new Domino());
-        // this.registrar(new Tilink());
-        // this.registrar(new Flowery())
-        // this.registrar(new Leon())
+        this.registrar(new Tilink());
+        this.registrar(new Flowery())
+        this.registrar(new Leon())
         this.registrar(new Kazuma())
-        // this.registrar(new Leah())
-        // this.registrar(new Robin())
-        // this.registrar(new Luciergana())
-        // this.registrar(new Haley())
-        // this.registrar(new Maggey())
+        this.registrar(new Leah())
+        this.registrar(new Robin())
+        this.registrar(new Luciergana())
+        this.registrar(new Haley())
+        this.registrar(new Maggey())
     }
 
     private registrar(p: IPersonaje) {
