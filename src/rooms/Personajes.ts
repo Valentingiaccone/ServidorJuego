330ed5c6c70a8a0f3195ef5f267d73a7aecab6c0
Valentingiaccone <163468423+Valentingiaccone@users.noreply.github.com>
2026-08-24 01:11:47 -0300

    listo para jugar

--- src/rooms/Personajes.ts
+++ src/rooms/Personajes.ts
@@ -1143,38 +1143,46 @@ export class Maya implements IPersonaje {
     }
 }
 
+export class Geraldo implements IPersonaje {
+    nombre = "Geraldo";
+    habilidad = "Criatura de la noche:\nAl recibir daño, gana una carta fantasmal aleatoria, cuando muere otro jugador, gana 2 cartas fantasmales aleatorias.";
+    habilidadEnCatalan = "Criatura de la nit:\nEn rebre dany, guanya una carta fantasmal aleatòria; quan mor un altre jugador, guanya 2 cartes fantasmals aleatòries.";
+    vidasBase = 4;
+
+}
+
 // 3. EL GESTOR DE PERSONAJES
 export class GestorPersonajes {
     private personajes: Record<string, IPersonaje> = {};
 
     constructor() {
-        // this.registrar(new ColeCasiddy());
-        // this.registrar(new Berry());
-        // this.registrar(new Maton());
-        // this.registrar(new Mandy());
-        // this.registrar(new Tralalero());
-        // this.registrar(new Darryl());
-        // this.registrar(new JetpackCat());
-        // this.registrar(new KayFaraday());
+        this.registrar(new ColeCasiddy());
+        this.registrar(new Berry());
+        this.registrar(new Maton());
+        this.registrar(new Mandy());
+        this.registrar(new Tralalero());
+        this.registrar(new Darryl());
+        this.registrar(new JetpackCat());
+        this.registrar(new KayFaraday());
         this.registrar(new Chester());
-        // this.registrar(new Frank());
-        // this.registrar(new Pam());
-        // this.registrar(new Trucy());
-        // this.registrar(new HongoUp());
-        // this.registrar(new Hongo());
-        // this.registrar(new Lesly());
-        // this.registrar(new Mikotoba());
-        // this.registrar(new Domino());
-        // this.registrar(new Tilink());
-        // this.registrar(new Flowery())
-        // this.registrar(new Leon())
-        // this.registrar(new Kazuma())
-        // this.registrar(new Leah())
-        // this.registrar(new Robin())
-        // this.registrar(new Luciergana())
-        // this.registrar(new Haley())
+        this.registrar(new Frank());
+        this.registrar(new Pam());
+        this.registrar(new Trucy());
+        this.registrar(new HongoUp());
+        this.registrar(new Hongo());
+        this.registrar(new Lesly());
+        this.registrar(new Mikotoba());
+        this.registrar(new Domino());
+        this.registrar(new Tilink());
+        this.registrar(new Flowery())
+        this.registrar(new Leon())
+        this.registrar(new Kazuma())
+        this.registrar(new Leah())
+        this.registrar(new Robin())
+        this.registrar(new Luciergana())
+        this.registrar(new Haley())
         this.registrar(new Maggey())
-        // this.registrar(new Mortis())
+        this.registrar(new Mortis())
         this.registrar(new Maya())
     }
 
